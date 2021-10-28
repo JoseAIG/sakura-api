3ea4470377a6764078d205465394cdad7bb8d281
JoseAIG <inciartegj@gmail.com>
2021-10-28 16:43:04 -0400

    User register and login
    Created routes/auth.py (Flask blueprint) and registered in app.py
    Created controllers/auth_controller.py (Auth logic)
    Created models/user.py
    Created helpers/jwt_tools.py (JWT functions)

---
+++ src/models/user.py
@@ -0,0 +1,15 @@
+from database import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), nullable=False, unique=True)
+    email = db.Column(db.String(60), nullable=False, unique=True)
+    password = db.Column(db.String(65), nullable=False)
+
+    # CONSTRUCTOR
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
